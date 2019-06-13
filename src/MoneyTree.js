2686e1bcd9c2d68c1e2006f2a7665fd01632edb1
CVVPK <cvaldizon93@gmail.com>
2019-06-13 09:56:47 -0400

    rm MoneyTree

--- src/MoneyTree.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function MoneyTree({ values, current }) {
-    function setCurrent(item) {
-        if (item === current) return "current";
-    }
-
-    return <span>Points: {values[current]}</span>;
-}
