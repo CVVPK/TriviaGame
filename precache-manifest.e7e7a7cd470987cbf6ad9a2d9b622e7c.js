32587ba802e779c7eaf354ae85013356753117a2
CVVPK <cvaldizon93@gmail.com>
2019-06-17 13:32:21 -0400

    Updates

---
+++ precache-manifest.e7e7a7cd470987cbf6ad9a2d9b622e7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f96a436710d8cbc7b0a5a93196e490",
+    "url": "/TriviaGame/index.html"
+  },
+  {
+    "revision": "e8da3a7e91086b0d23b6",
+    "url": "/TriviaGame/static/css/main.fa1eaf83.chunk.css"
+  },
+  {
+    "revision": "4b0b870bf6304ebe6e0d",
+    "url": "/TriviaGame/static/js/2.589b9d23.chunk.js"
+  },
+  {
+    "revision": "e8da3a7e91086b0d23b6",
+    "url": "/TriviaGame/static/js/main.bcd2debb.chunk.js"
+  },
+  {
+    "revision": "24e515022d386099b568",
+    "url": "/TriviaGame/static/js/runtime~main.ddda597e.js"
+  }
+]);
